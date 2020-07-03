74af47cc1e0fcdb6a5371946792c5837bb475352
Ammar <amr.tahhan@gmail.com>
2020-07-03 14:22:09 +0300

    Initial Commit

---
+++ Activity 10/API/APICalls.swift
@@ -0,0 +1,113 @@
+//
+//  APICalls.swift
+//  Activity 10 Solution
+//
+//  Created by Dania A on 13/11/2018.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+
+class APICalls {
+    
+    private static var accountKey = ""
+    
+    static func login (_ username : String!, _ password : String!, completion: @escaping (Bool, String, Error?)->()) {
+        
+        var request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/session")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "{\"udacity\": {\"username\": \"\(username!)\", \"password\": \"\(password!)\"}}".data(using: .utf8)
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            if error != nil {
+                // TODO: Call the completion handler and send the error so it can be handled on the UI, also call "return" so the code next to this block won't be executed
+                completion(false, "", error)
+            }
+            
+            //Get the status code to check if the response is OK or not
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
+                
+                // TODO: Call the completion handler and send the error so it can be handled on the UI, also call "return" so the code next to this block won't be executed (you need to call return in let guard's else body anyway)
+                completion(false, "", error)
+                return
+            }
+            
+            if statusCode >= 200  && statusCode < 300 {
+                
+                //Skipping the first 5 characters
+                let range = Range(5..<data!.count)
+                let newData = data?.subdata(in: range) /* subset response data! */
+                
+                //Print the data to see it and know you'll parse it (this can be removed after you complete building the app)
+                debugPrint(String(data: newData!, encoding: .utf8)!)
+                
+                //TODO: Initialize a JSONDecoder object
+                let decoder = JSONDecoder()
+                
+                //TODO: Convert the newData! to a User.self object
+                do {
+                    let user = try decoder.decode(User.self, from: newData!)
+                    //Get the unique key of the user
+                    let uniqueKey = user.account?.key ?? ""
+                    APICalls.accountKey = uniqueKey
+                    completion (true, uniqueKey, nil)
+                } catch {
+                    debugPrint(error)
+                    completion (false, "nil", nil)
+                }
+                
+            } else {
+                //TODO: call the completion handler properly
+                completion(false, "", NSError(domain: "status_code", code: statusCode, userInfo: nil))
+            }
+        }
+        //Start the task
+        task.resume()
+    }
+    
+    static func getAllLocations (completion: @escaping ([StudentLocation]?, Error?) -> ()) {
+        var request = URLRequest (url: URL (string: "https://onthemap-api.udacity.com/v1/StudentLocation?limit=100&order=-updatedAt")!)
+        
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) {data, response, error in
+            if error != nil {
+                // TODO: Call the completion handler and send the error so it can be handled on the UI, also call "return" so the code next to this block won't be executed
+                completion(nil, error)
+            }
+            
+            //Print the data to see it and know you'll parse it (this can be removed after you complete building the app)
+            debugPrint(String(data: data!, encoding: .utf8)!)
+            
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
+                // TODO: Call the completion handler and send the error so it can be handled on the UI, also call "return" so the code next to this block won't be executed (you need to call return in let guard's else body anyway)
+                completion(nil, error)
+                return
+            }
+            
+            if statusCode >= 200 && statusCode < 300 {
+                
+                //Get an object based on the received data in JSON format
+                let decoder = JSONDecoder()
+                
+                //TODO: Convert data to an object (you should create a model that holds an array of [StudentLocation])
+                do {
+                    let locationsResponse = try decoder.decode(StudentLocationsResult.self, from: data!)
+                    //TODO: call completion on the results
+                    completion(locationsResponse.results, nil)
+                } catch {
+                    debugPrint(error)
+                    completion(nil, error)
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
---
+++ Activity 10/VC/MapViewController.swift
@@ -0,0 +1,108 @@
+//
+//  MapViewController.swift
+//  Activity 10 Solution
+//
+//  Created by Dania A on 04/11/2018.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var MapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        MapView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        APICalls.getAllLocations () {(studentsLocations, error) in
+            DispatchQueue.main.async {
+                
+                if error != nil {
+                    let errorAlert = UIAlertController(title: "Erorr performing request", message: "There was an error performing your request", preferredStyle: .alert )
+                    
+                    errorAlert.addAction(UIAlertAction (title: "OK", style: .default, handler: { _ in
+                        return
+                    }))
+                    self.present(errorAlert, animated: true, completion: nil)
+                    return
+                }
+                
+                var annotations = [MKPointAnnotation] ()
+                
+                guard let locationsArray = studentsLocations else {
+                    let locationsErrorAlert = UIAlertController(title: "Erorr loading locations", message: "There was an error loading locations", preferredStyle: .alert )
+                    
+                    locationsErrorAlert.addAction(UIAlertAction (title: "OK", style: .default, handler: { _ in
+                        return
+                    }))
+                    self.present(locationsErrorAlert, animated: true, completion: nil)
+                    return
+                }
+                
+                //Loop through the array of structs and get locations data from it so they can be displayed on the map
+                for locationStruct in locationsArray {
+                    
+                    let long = CLLocationDegrees (locationStruct.longitude ?? 0)
+                    let lat = CLLocationDegrees (locationStruct.latitude ?? 0)
+                    
+                    let coords = CLLocationCoordinate2D (latitude: lat, longitude: long)
+                    
+                    //TODO: Get the media URL and call it mediaURL, if it's nil its value should be " ", for that use Nil-Coalescing Operator (??)
+                    let mediaURL = locationStruct.mediaURL ?? ""
+                    
+                    //TODO: Get the first name and call it first, if it's nil its value should be " ", for that use Nil-Coalescing Operator (??)
+                    let first = locationStruct.firstName ?? ""
+                    
+                    //TODO: Get the last name and call it last, if it's nil its value should be " ", for that use Nil-Coalescing Operator (??)
+                    let last = locationStruct.lastName ?? ""
+                    
+                    // Here we create the annotation and set its coordiate, title, and subtitle properties
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = coords
+                    annotation.title = "\(first) \(last)"
+                    annotation.subtitle = mediaURL
+                    
+                    annotations.append (annotation)
+                }
+                self.MapView.addAnnotations (annotations)
+            }
+            
+        }//end getAllLocations
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    // This delegate method is implemented to respond to taps. It opens the system browser
+    // to the URL specified in the annotationViews subtitle property.
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.shared
+            if let toOpen = view.annotation?.subtitle! {
+                app.open(URL(string: toOpen)!, options: [:], completionHandler: nil)
+            }
+        }
+    }
+}
